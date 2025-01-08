c5973f8633ec7856c765923309212888a03cbe23
buddhini <wanasundarabuddhini@gmail.com>
2025-01-08 19:56:44 +0530

    Temperature and Humidity monitoring DHT22

---
+++ H_and_T_monitor.ino
@@ -0,0 +1,73 @@
+#include "arduino_secrets.h"
+#include <DHT.h>
+#include <DHT_U.h>
+
+#include "thingProperties.h"
+#include "DHT.h"
+
+#define DHTPIN 4    
+#define DHTTYPE DHT22 
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+
+  Serial.begin(9600);
+
+  delay(1500); 
+
+  Serial.println("DHT22 test!");
+  dht.begin();
+  // Defined in thingProperties.h
+  initProperties();
+  // Connect to Arduino IoT Cloud
+  ArduinoCloud.begin(ArduinoIoTPreferredConnection);
+  
+  setDebugMessageLevel(2);
+  ArduinoCloud.printDebugInfo();
+}
+
+void loop() {
+  ArduinoCloud.update();
+  // Your code here 
+  float hum = dht.readHumidity();
+  float temp = dht.readTemperature();
+
+  if (isnan(humidity) || isnan(temperature)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  humidity = hum;
+  temperature = temp;
+  message = "Temperature: "+String(temp)+"  Humidity: "+String(hum);
+
+  Serial.print("Humidity: ");
+  Serial.print(hum);
+  Serial.print("%  Temperature: ");
+  Serial.print(temp);
+  Serial.println("°C");
+  delay(2000); // Wait a bit between readings
+}
+
+
+/*
+  Since Humidity is READ_WRITE variable, onHumidityChange() is
+  executed every time a new value is received from IoT Cloud.
+*/
+void onHumidityChange()  {
+  // Add your code here to act upon Humidity change
+}
+
+
+void onTemperatureChange()  {
+  // Add your code here to act upon Temperature change
+}
+
+/*
+  Since Message is READ_WRITE variable, onMessageChange() is
+  executed every time a new value is received from IoT Cloud.
+*/
+void onMessageChange()  {
+  // Add your code here to act upon Message change
+}
